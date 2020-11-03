d19b39a5c4cefc19f08371f3a6aa169d5a14a742
Mau Munguia <maumg1196@gmail.com>
2020-11-02 19:23:07 -0600

    Add enheancement process after creation

--- api/images/serializers.py
+++ api/images/serializers.py
@@ -1,10 +1,28 @@
 """Images Serializers."""
 
+# OS
+import os
+
+# Django
+from django.core.files import File
+
 # Django REST Framework
 from rest_framework import serializers
 
 # Model
-from .models import OriginalImage
+from .models import (
+    OriginalImage,
+    EnhancementImage,
+)
+
+# Scikit Image
+from skimage import (
+    color,
+    io,
+    exposure,
+)
+
+import numpy as np
 
 
 class OriginalImageSerializer(serializers.ModelSerializer):
@@ -20,6 +38,50 @@ class OriginalImageSerializer(serializers.ModelSerializer):
         )
 
     def create(self, validated_data):
-    	print(type(validated_data['image']))
-    	new_original_image = OriginalImage.objects.create(**validated_data)
-    	return new_original_image
+        new_original_image = OriginalImage.objects.create(**validated_data)
+        img = np.uint8(
+            color.rgb2gray(
+                io.imread(
+                    f'{new_original_image.image.file}')
+                ) * 255
+            )
+        name = str(new_original_image.image)
+        
+        gc_name = name.replace('original_images/','gc_')
+        eq_name = name.replace('original_images/','eq_')
+        adapteq_name = name.replace('original_images/','adpteq_')
+        
+        gc = exposure.adjust_gamma(img, 2)
+        eq = exposure.equalize_hist(img)
+        adapteq = exposure.equalize_adapthist(img, clip_limit=0.05)
+
+        io.imsave(gc_name, gc)
+        io.imsave(eq_name, eq)
+        io.imsave(adapteq_name, adapteq)
+
+        gc_img = File(open(gc_name, 'rb'))
+        eq_img = File(open(eq_name, 'rb'))
+        adapteq_img = File(open(adapteq_name, 'rb'))
+
+        gamma_enhancement = EnhancementImage(
+            original=new_original_image,
+            method='Gamma Correction',
+        )
+        equalize_enhancement = EnhancementImage(
+            original=new_original_image,
+            method='Gamma Correction',
+        )
+        adaptative_enhancement = EnhancementImage(
+            original=new_original_image,
+            method='Gamma Correction',
+        )
+
+        gamma_enhancement.image.save(gc_name, gc_img)
+        equalize_enhancement.image.save(eq_name, eq_img)
+        adaptative_enhancement.image.save(adapteq_name, adapteq_img)
+
+        os.remove(gc_name)
+        os.remove(eq_name)
+        os.remove(adapteq_name)
+
+        return new_original_image
