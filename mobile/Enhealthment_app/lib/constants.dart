4ee4a9e959ff16b8b9843638873f7af5dbf76ff6
AntonioDominguez <petoperico@hotmail.com>
2020-11-05 10:07:47 -0600

    Add colors to constants.dart

---
+++ mobile/Enhealthment_app/lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xff00aaff);
+const kPrimaryBackgroundColor = Color(0xff202025);
+const kPrimaryTextColor = Color(0xff9a9a9f);
+const kPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter,
+  colors: [Color(0xff326eaf), Color(0xff98daec)],
+);
