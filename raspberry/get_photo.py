62f3c04e58e159b518d267685ee0cd1c56f8567b
Vinsmoke Mau <vinsmokemau@pop-os.localdomain>
2020-11-22 22:24:14 -0600

    Add option to send a saved image

--- raspberry/get_photo.py
+++ raspberry/get_photo.py
@@ -1,14 +1,24 @@
 import cv2
 import json
 import requests
-from base64 import b64encode
 
 
-webcam = cv2.VideoCapture(0)
-check, frame = webcam.read()
-cv2.imwrite(filename='saved_img.png', img=frame)
-webcam.release()
-cv2.destroyAllWindows()
+option = input("""Si desea subir una foto guardada ingrese (0)
+
+Si desea tomar una foto con la cámara ingrese (1)
+
+
+------>   """)
+
+if '0' == option:
+    image_name = input('Ingresa el nombre del archivo con todo y extensión: ')
+elif '1' == option:
+    webcam = cv2.VideoCapture(0)
+    check, frame = webcam.read()
+    cv2.imwrite(filename='saved_img.png', img=frame)
+    webcam.release()
+    cv2.destroyAllWindows()
+    image_name = 'saved_img.png'
 
 endpoint = 'http://164.90.146.236:8000/api/v1.0/images/'
 
@@ -17,7 +27,7 @@ data = {
 }
 
 files = {
-    'image': open('saved_img.png', 'rb'),
+    'image': open(image_name, 'rb'),
 }
 
 r = requests.post(url=endpoint, data=data, files=files)
